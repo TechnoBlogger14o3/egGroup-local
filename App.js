afbe2c8ed211ad058d0ded77c236907283ad691d
winit30 <vineetm616@gmail.com>
2018-10-15 18:38:53 +0530

    linting added

--- App.js
+++ App.js
@@ -4,80 +4,92 @@
 * @summary Starting point of the application
 */
 
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import React, {Component} from 'react';
-import {Platform} from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
-import PushNotification from 'react-native-push-notification';
+// import - npm modules
+import {PersistGate} from "redux-persist/integration/react";
+import {Provider} from "react-redux";
+import React, {Component} from "react";
+import {Platform} from "react-native";
+import SplashScreen from "react-native-splash-screen";
+import PushNotification from "react-native-push-notification";
 
-import Main from './src/Main';
-import store from './src/config/store';
+// import - custom classes
+import Main from "./src/Main";
+import store from "./src/config/store";
 
+/**
+* @returns {object}
+*/
 const persist = store();
 
 /**
 * Represents App.
-* @constructor
+* @class App
+* @extends Component
 */
 class App extends Component {
 
-   componentDidMount() {
+    /**
+    * @function componentDidMount
+    * React lifecycle method
+    */
+    componentDidMount() {
 
-    PushNotification.configure({
+        PushNotification.configure({
 
-        // (optional) Called when Token is generated (iOS and Android)
-        onRegister: function(token) {
-            console.log( 'TOKEN:', token );
-        },
+            // (optional) Called when Token is generated (iOS and Android)
+            onRegister: (token) => {
+                console.log("TOKEN:", token);
+            },
 
-        // (required) Called when a remote or local notification is opened or received
-        onNotification: function(notification) {
-            console.log( 'NOTIFICATION:', notification );
+            // (required) Called when a remote or local notification is opened or received
+            onNotification: (notification) => {
+                console.log("NOTIFICATION:", notification);
+                // process the notification
+                // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+                notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
 
-            // process the notification
+            // ANDROID ONLY: GCM or FCM Sender ID (product_number) (optional - not required for local notifications, but is need to receive remote push notifications)
+            senderID: "YOUR GCM (OR FCM) SENDER ID",
 
-            // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
-            notification.finish(PushNotificationIOS.FetchResult.NoData);
-        },
+            // IOS ONLY (optional): default: all - Permissions to register.
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true
+            },
 
-        // ANDROID ONLY: GCM or FCM Sender ID (product_number) (optional - not required for local notifications, but is need to receive remote push notifications)
-        senderID: "YOUR GCM (OR FCM) SENDER ID",
+            // Should the initial notification be popped automatically
+            // default: true
+            popInitialNotification: true,
 
-        // IOS ONLY (optional): default: all - Permissions to register.
-        permissions: {
-            alert: true,
-            badge: true,
-            sound: true
-        },
-
-        // Should the initial notification be popped automatically
-        // default: true
-        popInitialNotification: true,
-
-        /**
-          * (optional) default: true
-          * - Specified if permissions (ios) and token (android and ios) will requested or not,
-          * - if not, you must call PushNotificationsHandler.requestPermissions() later
-          */
-        requestPermissions: true,
-    });
-        setTimeout(function(){
-          if (Platform.OS !== 'ios') {
-            SplashScreen.hide()
-          }
+            /**
+              * (optional) default: true
+              * - Specified if permissions (ios) and token (android and ios) will requested or not,
+              * - if not, you must call PushNotificationsHandler.requestPermissions() later
+              */
+            requestPermissions: true
+        });
+        setTimeout(() => {
+            if (Platform.OS !== "ios") {
+                SplashScreen.hide();
+            }
         }, 2000);
-  }
+    }
 
-  render(){
-    return(
-      <Provider store={persist.store}>
-        <PersistGate loading={null} persistor={persist.persistor}>
-          <Main />
-        </PersistGate>
-      </Provider>
-    )
-  }
+    /**
+    * @function render
+    * React render method for rendering the native elements
+    */
+    render() {
+        return (
+            <Provider store={persist.store}>
+                <PersistGate loading={null} persistor={persist.persistor}>
+                    <Main />
+                </PersistGate>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,57 @@
+/**
+* @author Vineet Mishra <vineet.m@photoninfotech.net>
+* @version 1.0.0
+* @summary Adding routes component and status bar
+*/
+
+// Import npm modules
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { StatusBar, View } from 'react-native';
 
+// Import custom classes
 import Routes from './components/Routes/index';
 
+// Import styles
 import styles from './styles';
 
+/**
+* Represents Main class.
+* @class Main
+* @extends Component
+*/
 class Main extends Component {
+  /**
+  * @function render
+  * React render method for rendering the native elements
+  */
   render() {
-    return (
-      <View style={styles.appContainer}>
-        <StatusBar
-          backgroundColor="#ffffff"
-          barStyle="dark-content"
-        />
-        <Routes />
-      </View>
-    );
+      return (
+          <View style={styles.appContainer}>
+            <StatusBar
+              backgroundColor="#ffffff"
+              barStyle="dark-content"
+            />
+            <Routes />
+          </View>
+      );
   }
 }
 
+/**
+* Converting redux state to props for the Main component
+* @function mapStateToProps
+* @params {object} state
+* @returns {object} props
+*/
 const mapStateToProps = () => ({});
+
+/**
+* Converting redux state to props for the Main component
+* @function mapDispatchToProps
+* @params {function} dispatch
+* @returns {object} props
+*/
 const mapDispatchToProps = () => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
